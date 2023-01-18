8f699de16b36c6179e1cbc1fb380b40fe3c11960
Yered Peronnet <pompote@pop-os.localdomain>
2023-01-18 01:47:34 +0100

    map parsed

--- src/extract_map.c
+++ src/extract_map.c
@@ -6,7 +6,7 @@
 /*   By: pompote <pompote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 15:16:21 by yperonne          #+#    #+#             */
-/*   Updated: 2023/01/17 22:01:34 by pompote          ###   ########.fr       */
+/*   Updated: 2023/01/18 01:12:48 by pompote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ void	add_new_map_line(t_map **map, t_map *new_map_line_elem)
 }
 
 /* Check if linked list is up */
-/*void	linkedlist_check(int i, t_map **map)
+void	linkedlist_check(int i, t_map **map)
 {
 	int	r;
 
@@ -64,7 +64,28 @@ void	add_new_map_line(t_map **map, t_map *new_map_line_elem)
 		}
 	}
 }
-*/
+
+void	line_break_suppression(t_map **map)
+{
+	int	i;
+
+	i = 0;
+	if (map)
+	{
+		while (*map)
+		{
+			i = 0;
+			while ((*map)->map_line[i])
+			{
+				if ((*map)->map_line[i] == '\n')
+					(*map)->map_line[i] = '\0';
+				i++;
+			}
+			map = &(*map)->next;
+		}
+	}
+}
+
 /*extract the line of the map file and do a linkedlist of all element*/
 t_map	*extract_map(char **argv)
 {
@@ -84,9 +105,11 @@ t_map	*extract_map(char **argv)
 			add_new_map_line(&map, new_map_line(get_next_line(map_file)));
 		if (!map->map_line || !read(map_file, buf, 1)) /*on Macosx !read(map_file, 0, 1), on linux (!read(map_file, buf, 1))*/
 			break ;
+		lseek(map_file, -1, SEEK_CUR);
 		i++;
 	}
-//	linkedlist_check(i, &map);
+	line_break_suppression(&map);
+	linkedlist_check(i, &map);
 	close(map_file);
 	return (map);
 }
--- src/map_parsing_check.c
+++ src/map_parsing_check.c
@@ -6,7 +6,7 @@
 /*   By: pompote <pompote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:57:10 by pompote           #+#    #+#             */
-/*   Updated: 2023/01/17 23:54:04 by pompote          ###   ########.fr       */
+/*   Updated: 2023/01/18 01:45:17 by pompote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 static int	check_line_size(t_map *map, t_elems *elems)
 {
-	elems->line_size = ft_strlen(map->map_line) - 1; // -1 car ft=strlen ajoute un à la première ligne du .ber
+	elems->line_size = ft_strlen(map->map_line);
 	map = map->next;
 	while (map)
 	{
-		if (map->next == NULL)
-			elems->line_size -= 1; // je décremente line_size car ft_strlen compte un de moins pour la derniere ligne
+//		if (map->next == NULL)
+//			elems->line_size -= 1; // je décremente line_size car ft_strlen compte un de moins pour la derniere ligne
 		if (ft_strlen(map->map_line) != elems->line_size)
 			error_log("Error : different line size\n");
 		else
@@ -32,13 +32,11 @@ static void	check_1(char *str, t_elems *elems)
 {
 	if (elems->first_line == 0)
 		elems->first_line += 1;
-	ft_putstr(str);
 	while (*str)
 	{
 		if (*str != '1')
-		{	//error_log("Error : first or last line not only composed of 1\n");
-			ft_putstr("Error Line 1\n");
-			printf("%c\n", *str);
+		{
+			error_log("Error : first or last line not only composed of 1\n");
 		}
 		str++;
 	}
@@ -49,21 +47,27 @@ static void	check_interline(char *str, t_elems *elems)
 	int	i;
 
 	i = 1;
-	if (str[0] != '1' || str[elems->line_size] != 1)
-		error_log("Error : first or last letter != 1\n");
-	while (str[i])
+	if ((str[0] == '1') && (str[elems->line_size - 1] == '1'))
 	{
-		if (str[i] != 'E' || str[i] != 'C' || str[i] != 'P'
-			|| str[i] != '0' || str[i] != '1')
-			error_log("Error : unauthorized letter\n");
-		else if (str[i] == 'E')
-			elems->e += 1;
-		else if (str[i] == 'C')
-			elems->c += 1;
-		else if (str[i] == 'P')
-			elems->c += 1;
-		i++;
+		while (str[i])
+		{
+			if (str[i] == 'E' || str[i] == 'C' || str[i] == 'P'
+				|| str[i] == '0' || str[i] == '1')
+			{
+				if (str[i] == 'E')
+					elems->e += 1;
+				else if (str[i] == 'C')
+					elems->c += 1;
+				else if (str[i] == 'P')
+					elems->p += 1;
+			}
+			else
+				error_log("Error : unauthorized letter\n");
+			i++;
+		}
 	}
+	else
+		error_log("Error : first or last letter != 1\n");
 }
 
 static int	check_1_square(t_map *map, t_elems *elems)
@@ -80,6 +84,10 @@ static int	check_1_square(t_map *map, t_elems *elems)
 			error_log("Error : too many 'exit' or 'spawn point'\n");
 		map = map->next;
 	}
+	if (elems->e < 1)
+		error_log("Error : need one exit\n");
+	if (elems->p < 1)
+		error_log("Error : need one spawn point\n");
 	return (1);
 }
 
@@ -87,5 +95,6 @@ int	check_squared_and_lines(t_map **map, t_elems **elems)
 {
 	check_line_size(*map, *elems);
 	check_1_square(*map, *elems);
+
 	return (1);
 }
